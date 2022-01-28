de183f47ef565e25188abec6db4dc2fa4251ad21
karthik617 <yadavkarthik617@gmail.com>
2022-01-28 22:04:11 +0530

    changes made on QuestionBox.js

--- quizapp/src/Components/QuestionBox.js
+++ quizapp/src/Components/QuestionBox.js
@@ -2,12 +2,11 @@ import React,{useState,useEffect} from 'react';
 import '../Styles/QuestionBox.css'
 
 function QuestionBox({question,option,selected}) {
-  console.log("Question:"+question)
-  const [q,setQ] = useState(question)
+  // console.log("Question:"+question)
+  // const [q,setQ] = useState(question)
   const [opt,setOpt] = useState(option)
-  console.log("q:"+q)
   useEffect(()=>{
-    setQ(q)
+    // setQ(q)
     setOpt(option)
   },[option])
   return( 
